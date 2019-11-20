19696de32765501fa1e68c4f9e6a0c0028c48b6e
aydar <dusmukhambetov@mail.ru>
2019-11-20 11:26:04 +0300

    Add new tests

--- test/TestingSet.java
+++ test/TestingSet.java
@@ -54,4 +54,40 @@ public class TestingSet {
         Assert.assertTrue(set.containsAll(viceVersa));
         Assert.assertTrue(viceVersa.containsAll(set));
     }
+
+    @Test
+    public void testNotBelongBeforeAdd() {
+        Assert.assertFalse(set.contains(5));
+        set.add(5);
+        Assert.assertTrue(set.contains(5));
+    }
+
+    @Test
+    public void testRemoveNonExistingElement() {
+        set.remove(5);
+        Assert.assertEquals(3,set.size());
+    }
+
+    @Test
+    public void testRemoveExistingElement() {
+        set.remove(1);
+        Assert.assertEquals(2,set.size());
+    }
+
+    @Test
+    public void testRemoveAllSet() {
+        set.clear();
+        Assert.assertEquals(0,set.size());
+        Assert.assertTrue(set.isEmpty());
+    }
+
+    @Test
+    public void testIsSubset() {
+        HashSet<Integer> bigSet = new HashSet<>();
+        for (int i = 0; i < 100; i++) {
+            bigSet.add(i);
+        }
+        Assert.assertEquals(100, bigSet.size());
+        Assert.assertTrue(bigSet.containsAll(set));
+    }
 }
